dffc1ae03cf1441ea267d20f7de82f00215709fe
Sneha Rastogi <iit2023154@iiita.ac.in>
2024-12-23 16:59:49 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://SnehaRastogi:srnaeshta0606@sanjeevani.7s52d.mongodb.net/ecommerce?", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
   });
   console.log("Database Connected Successfully");
 } catch (err) {

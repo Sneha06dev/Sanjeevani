08903c0e746eabcfe9e0533e824a7f1244bf2048
Sneha Rastogi <iit2023154@iiita.ac.in>
2024-12-23 17:00:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
   })
   .then(() =>
     console.log(
